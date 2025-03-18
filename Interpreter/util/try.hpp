e4a6e66a615f50d50d0eff868e7cd39f8bed07f9
Kevin Ingles <elkevin111@gmail.com>
2025-03-17 22:08:43 -0400

    Remove bug in try macro

--- Interpreter/util/try.hpp
+++ Interpreter/util/try.hpp
@@ -4,12 +4,13 @@
 
 #include "print.hpp"
 
+// TODO: Add error message to error message, once me move to fmtlib
 #define TRY(result)                                                                               \
     ({                                                                                            \
         auto expression{ result };                                                                \
         if (expression.is_error()) {                                                              \
             printf("\033[31m[Error] %s:%s:%d\n\033[0m", __FILE__, #result, __LINE__);             \
-            return expression.error());                                                           \
+            return expression.error();                                                            \
         }                                                                                         \
         std::move(expression.release());                                                          \
     })
