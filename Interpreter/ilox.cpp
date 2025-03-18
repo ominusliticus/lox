b271fb9b1b8475ab130be994d0b9e53238aa9e01
Kevin Ingles <elkevin111@gmail.com>
2025-03-17 22:10:19 -0400

    Add cmdln parsing capabilities

--- Interpreter/ilox.cpp
+++ Interpreter/ilox.cpp
@@ -1,35 +1,58 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
-#include "util/print.hpp"
 #include "util/error.hpp"
+#include "util/print.hpp"
 #include "util/try.hpp"
+
+#include "internals/token.hpp"
+
+// Reads and lexes file
+auto 
+run(
+    std::istream&& buf
+) -> ErrorOr<void> {
+    (void)buf;
+    return {};
+}
 
 // Runs lox interpreter on file
 auto 
 run_file(
-    char const* file_name
+    char const *file_name
 ) -> ErrorOr<void> {
-    std::filesystem::path file{ file_name };
-    if (std::filesystem::exists(file)) {
-        return {};
-    }
-    else { 
+    std::filesystem::path file_path{file_name};
+    if (std::filesystem::exists(file_path)) {
+        std::fstream file_stream{ file_path };
+        std::istream buf{ file_stream.rdbuf() };
+        return run(std::move(buf));
+    } else {
         return ErrorType::FILE_NOT_FOUND;
     }
+    return ErrorType::UNREACHABLE;
 }
 
 // Runs lox interpreter on command line
 auto
 run_prompt(
-) -> ErrorOr<void> {
+) -> ErrorOr<void> { 
+    std::string line;
+    while (true) {
+        printf("ilox> ");
+        if (!std::getline(std::cin, line) && !std::cin.eof()) return ErrorType::INVALID_COMMAND;
+        else if (line == "exit()" || std::cin.eof()) break;
+        std::stringstream buf{ std::move(line) };
+        TRY(run(std::move(buf)));
+    }
     return {};
 }
 
 auto
 main(
-    [[maybe_unused]] int argc, 
+    [[maybe_unused]] int argc,
     [[maybe_unused]] char **argv
 ) -> int {
     if (argc > 2) {
@@ -37,13 +60,10 @@ main(
         exit(64);
     } else if (argc == 2) {
         TRY_MAIN(run_file(argv[1]));
-    }
-    else {
+    } else {
         TRY_MAIN(run_prompt());
     }
 
-   
-    return 0;
+  return 0;
 }
-
 
